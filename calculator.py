e58319b1b92516da0322561b5dd0bd464f1db3ed
marcortiztorres <ortiztorresmarc@gmail.com>
2025-05-22 11:35:16 +0200

    Revert changes for the workshop

--- calculator.py
+++ calculator.py
@@ -1,9 +1,6 @@
 def add(x: int, y: int) -> int:
-    """Add two numbers"""
     return x + y
 
 def div(x, y) -> float:
-    """Divide two numbers"""
-    assert y != 0, "Cannot divide by zero"
     return x / y
 
