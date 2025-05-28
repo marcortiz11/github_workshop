97d1ff30e18726452a24bcd7254b09ba35687bf5
LeoGiacomet <leonardo@restb.ai>
2025-05-28 15:26:21 +0200

    added types, description to solve merge conflict

--- calculator.py
+++ calculator.py
@@ -1,8 +1,10 @@
 def add(x, y):
     return x + y
 
-def div(x, y):
+def div(x, y) -> float:
+    """Divide two integer values"""
     if (y == 0):
         raise ValueError("Division by zero")
     else:
         return x / y
+
