2d88dcf29a35f6748ebffe1e13749e8925e7cf3e
Ciarán Coakley <ciaran@restb.ai>
2025-05-28 11:55:32 +0200

    Adding doccumetation

--- calculator.py
+++ calculator.py
@@ -1,9 +1,9 @@
 def add(x: int, y: int) -> int:
-    """Add two numbers"""
+    """Add two integer values"""
     return x + y
 
 def div(x, y) -> float:
-    """Divide two numbers"""
+    """Divide two integer values"""
     assert y != 0, "Cannot divide by zero"
     return x / y
 
